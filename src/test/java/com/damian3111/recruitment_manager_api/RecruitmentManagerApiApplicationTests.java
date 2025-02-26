5e87147121edad254cf60b4d88178548237d8c00
damian3111 <damiankwasny311@gmail.com>
2025-02-26 11:47:06 +0100

    Initial commit

---
+++ src/test/java/com/damian3111/recruitment_manager_api/RecruitmentManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian3111.recruitment_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
