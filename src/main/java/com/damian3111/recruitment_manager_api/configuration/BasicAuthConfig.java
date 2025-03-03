2865262629daa0883d01715ea3fd87fde0ca2b0c
damian3111 <damiankwasny311@gmail.com>
2025-03-03 23:14:19 +0100

    add corsConfigurer

--- src/main/java/com/damian3111/recruitment_manager_api/configuration/BasicAuthConfig.java
+++ src/main/java/com/damian3111/recruitment_manager_api/configuration/BasicAuthConfig.java
@@ -21,6 +21,14 @@ public class BasicAuthConfig{
         });
         return http.build();
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 
 }
