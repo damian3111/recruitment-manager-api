8c691e44c3e14de844486e32d20bbce2b3190caf
damian3111 <damiankwasny311@gmail.com>
2025-03-15 03:03:42 +0100

    create modelMapper bean

---
+++ src/main/java/com/damian3111/recruitment_manager_api/configuration/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package com.damian3111.recruitment_manager_api.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
