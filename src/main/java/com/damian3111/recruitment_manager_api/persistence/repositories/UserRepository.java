03635196164adbdd89287e2ff83d4b1f2a27a2d7
damian3111 <damiankwasny311@gmail.com>
2025-03-16 03:06:39 +0100

    add user repo, service layer

---
+++ src/main/java/com/damian3111/recruitment_manager_api/persistence/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.damian3111.recruitment_manager_api.persistence.repositories;
+
+import com.damian3111.recruitment_manager_api.persistence.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserEntityByEmail(String email);
+}
---
+++ src/main/java/com/damian3111/recruitment_manager_api/services/UserService.java
@@ -0,0 +1,21 @@
+package com.damian3111.recruitment_manager_api.services;
+
+import com.damian3111.recruitment_manager_api.persistence.entities.UserEntity;
+import com.damian3111.recruitment_manager_api.persistence.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserEntity addUser(UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    public UserEntity getUserByEmail(String email) {
+        return userRepository.findUserEntityByEmail(user.getEmail()).orElseThrow();
+    }
+}
