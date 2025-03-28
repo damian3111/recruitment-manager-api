69478dd50ecc38eed895423f9eea2ec683289575
damian3111 <damiankwasny311@gmail.com>
2025-03-28 19:29:42 +0100

    authenticationProvider

--- src/main/java/com/damian3111/recruitment_manager_api/configuration/ApplicationConfiguration.java
+++ src/main/java/com/damian3111/recruitment_manager_api/configuration/ApplicationConfiguration.java
@@ -1,11 +1,44 @@
 package com.damian3111.recruitment_manager_api.configuration;
 
+import com.damian3111.recruitment_manager_api.persistence.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfiguration {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepository.findUserEntityByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
 
     @Bean
     public ModelMapper modelMapper() {
