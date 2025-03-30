23be556961b1d85a90df4086a19177ee49aa025d
damian3111 <damiankwasny311@gmail.com>
2025-03-30 20:36:16 +0200

    implement UserDetails

--- src/main/java/com/damian3111/recruitment_manager_api/persistence/entities/UserEntity.java
+++ src/main/java/com/damian3111/recruitment_manager_api/persistence/entities/UserEntity.java
@@ -2,6 +2,14 @@ package com.damian3111.recruitment_manager_api.persistence.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -10,7 +18,7 @@ import lombok.*;
 @AllArgsConstructor
 @Table(name = "users")
 @NoArgsConstructor
-public class UserEntity{
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
@@ -27,7 +35,48 @@ public class UserEntity{
     @Enumerated(value =  EnumType.STRING)
     private UserRole role;
 
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
     @ManyToOne
     @JoinColumn(name = "company_id")
     private CompanyEntity company;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
