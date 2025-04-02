42e9e3eb7f82838fedf5aff3e7dafed5560e60f9
damian3111 <damiankwasny311@gmail.com>
2025-04-02 20:39:22 +0200

    adjust UserService for JWT

--- src/main/java/com/damian3111/recruitment_manager_api/services/UserService.java
+++ src/main/java/com/damian3111/recruitment_manager_api/services/UserService.java
@@ -3,6 +3,12 @@ package com.damian3111.recruitment_manager_api.services;
 import com.damian3111.recruitment_manager_api.persistence.entities.UserEntity;
 import com.damian3111.recruitment_manager_api.persistence.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.openapitools.model.LoginUserDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,9 +16,21 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AuthenticationProvider authenticationProvider;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public UserEntity addUser(UserEntity user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public UserEntity authenticate(LoginUserDto loginUserDto) {
+        Authentication authentication = authenticationProvider.authenticate(
+                new UsernamePasswordAuthenticationToken(loginUserDto.getEmail(), loginUserDto.getPassword())
+        );
+
+        return userRepository.findUserEntityByEmail(loginUserDto.getEmail())
+                .orElseThrow();
     }
 
     public UserEntity getUserByEmail(String email) {
