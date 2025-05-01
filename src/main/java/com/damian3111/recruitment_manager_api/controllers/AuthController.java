b5e477f171531cdd62562dbe65e78a34501b3864
damian3111 <damiankwasny311@gmail.com>
2025-05-01 13:03:41 +0200

    AuthController login endpoint

--- src/main/java/com/damian3111/recruitment_manager_api/controllers/AuthController.java
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/AuthController.java
@@ -9,10 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -31,6 +28,5 @@ public class AuthController {
 
         return ResponseEntity.ok(jwtToken);
     }
-
 
 }
