cb6600a316a7d4fb2ec42628d6bd6255b2feec53
damian3111 <damiankwasny311@gmail.com>
2025-05-10 13:17:28 +0200

    EmailConfirmationTokenRepository

---
+++ src/main/java/com/damian3111/recruitment_manager_api/persistence/repositories/EmailConfirmationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.damian3111.recruitment_manager_api.persistence.repositories;
+
+import com.damian3111.recruitment_manager_api.persistence.entities.CandidateEntity;
+import com.damian3111.recruitment_manager_api.persistence.entities.EmailConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailConfirmationTokenRepository extends JpaRepository<EmailConfirmationToken, Long>{
+    Optional<EmailConfirmationToken> findByToken(String token);
+}
