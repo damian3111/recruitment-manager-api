b0065a81992cf2e6c39b9878fd853558c8c954e1
damian3111 <damiankwasny311@gmail.com>
2025-05-10 13:16:27 +0200

    add confirmEmail - EmailController

---
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/EmailController.java
@@ -0,0 +1,38 @@
+package com.damian3111.recruitment_manager_api.controllers;
+
+import com.damian3111.recruitment_manager_api.persistence.entities.EmailConfirmationToken;
+import com.damian3111.recruitment_manager_api.persistence.entities.UserEntity;
+import com.damian3111.recruitment_manager_api.persistence.repositories.EmailConfirmationTokenRepository;
+import com.damian3111.recruitment_manager_api.persistence.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.openapitools.api.EmailApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class EmailController implements EmailApi {
+
+    private final EmailConfirmationTokenRepository emailConfirmationTokenRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public ResponseEntity<String> confirmEmail(String token) {
+        EmailConfirmationToken confirmationToken = emailConfirmationTokenRepository.findByToken(token)
+                .orElseThrow(() -> new RuntimeException("Invalid token"));
+
+        if (confirmationToken.getExpiresAt().isBefore(LocalDateTime.now())) {
+            return ResponseEntity.badRequest().body("Token expired");
+        }
+
+        UserEntity user = confirmationToken.getUser();
+        user.setEmailConfirmed(true);
+        userRepository.save(user);
+
+        emailConfirmationTokenRepository.delete(confirmationToken);
+
+        return ResponseEntity.ok("Email confirmed");    }
+}
+
