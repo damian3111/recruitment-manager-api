b80f3854bd148700310d68a8535e9a4ed087dcda
damian3111 <damiankwasny311@gmail.com>
2025-05-10 13:15:55 +0200

    CustomMailConfig

---
+++ src/main/java/com/damian3111/recruitment_manager_api/configuration/CustomMailConfig.java
@@ -0,0 +1,36 @@
+package com.damian3111.recruitment_manager_api.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class CustomMailConfig {
+    @Value("${spring.mail.email}")
+    private String senderEmail;
+
+    @Value("${spring.mail.password}")
+    private String senderPassword;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(senderEmail);
+        mailSender.setPassword(senderPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
