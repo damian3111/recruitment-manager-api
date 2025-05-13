c886957fc54d8643b244f9c182e09d9be22c7c62
damian3111 <damiankwasny311@gmail.com>
2025-05-13 13:55:37 +0200

    add JobController and UserController

--- src/main/java/com/damian3111/recruitment_manager_api/controllers/JobController.java
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/JobController.java
@@ -9,7 +9,9 @@ import org.openapitools.model.JobDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,5 +26,39 @@ public class JobController implements JobsApi {
         List<JobEntity> all = jobRepository.findAll();
         List<JobDto> collect = all.stream().map(j -> modelMapper.map(j, JobDto.class)).toList();
         return ResponseEntity.ok(collect);
+    }
+
+    @Override
+    public ResponseEntity<List<JobDto>> getJobsByUserId(Long userId) {
+        List<JobEntity> jobEntities = jobRepository.findByUserId(userId).orElseThrow();
+        List<JobDto> parsedJobEntities = jobEntities.stream()
+                .map(jobEntity -> modelMapper.map(jobEntity, JobDto.class)).collect(Collectors.toList());
+        return ResponseEntity.ok(parsedJobEntities);
+    }
+
+    @Override
+    public ResponseEntity<JobDto> createJob(JobDto jobDto) {
+        JobEntity map = modelMapper.map(jobDto, JobEntity.class);
+        map.setCompanyName("sdds");
+        map.setPostedDate(LocalDate.now());
+        map.setApplicationDeadline(LocalDate.now());
+        JobEntity save = jobRepository.save(map);
+
+        return ResponseEntity.ok(jobDto);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteJob(Long id) {
+        return JobsApi.super.deleteJob(id);
+    }
+
+    @Override
+    public ResponseEntity<JobDto> getJobById(Long id) {
+        return ResponseEntity.ok(modelMapper.map(jobRepository.findById(id).orElseThrow(), JobDto.class));
+    }
+
+    @Override
+    public ResponseEntity<JobDto> updateJob(Long id, JobDto jobDto) {
+        return JobsApi.super.updateJob(id, jobDto);
     }
 }
--- src/main/java/com/damian3111/recruitment_manager_api/controllers/UserController.java
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/UserController.java
@@ -7,8 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.openapitools.api.UsersApi;
 import org.openapitools.model.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,9 +45,14 @@ public class UserController implements UsersApi {
         return null;
     }
 
-    //    @Override
-//    public ResponseEntity<User> addUser(User user) {
-//        System.out.println("@@@@@@@@@@@@@@@");
-//        return ResponseEntity.ok(new User());
-//    }
+    @Override
+    public ResponseEntity<User> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String email = authentication.getName();
+        return ResponseEntity.ok(modelMapper.map(userService.getUserByEmail(email), User.class));
+    }
 }
