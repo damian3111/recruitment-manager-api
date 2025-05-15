3de63b22beca0c85e25bd585f93ece360a477583
damian3111 <damiankwasny311@gmail.com>
2025-05-15 13:50:33 +0200

    creation CandidateSpecification

---
+++ src/main/java/com/damian3111/recruitment_manager_api/persistence/specification/CandidatesSpecification.java
@@ -0,0 +1,31 @@
+package com.damian3111.recruitment_manager_api.persistence.specification;
+
+import com.damian3111.recruitment_manager_api.persistence.entities.CandidateEntity;
+import jakarta.persistence.criteria.*;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.Iterator;
+
+@Component
+public class CandidatesSpecification implements BaseSpecification<CandidateEntity, Long>{
+
+//    public Specification<CandidateEntity> beforeAppliedDate(String path, String search) {
+//        return (root, cq, cb) -> {
+//            Path<?> pathToObject = this.resolvePathToObject(root, path);
+//            return this.getPredicateBeforeAppliedDate(cb, pathToObject, search);
+//        };
+//    }
+//    private Predicate getPredicateBeforeAppliedDate(CriteriaBuilder cb, Path<?> path, String search) {
+//            Expression<String> dateStringExpr = cb.function("TO_CHAR", String.class, path, cb.literal("'YYYY-MM-DD'"));
+//            return cb.like(cb.lower(dateStringExpr), this.getLowerCaseSearchTerm(search));
+//    }
+
+
+
+}
