01882a7301c41540991b6231f17952aab67bcaf1
damian3111 <damiankwasny311@gmail.com>
2025-05-15 13:51:34 +0200

    remove comments CandidateSpecification

--- src/main/java/com/damian3111/recruitment_manager_api/persistence/specification/CandidatesSpecification.java
+++ src/main/java/com/damian3111/recruitment_manager_api/persistence/specification/CandidatesSpecification.java
@@ -15,18 +15,6 @@ import java.util.Iterator;
 @Component
 public class CandidatesSpecification implements BaseSpecification<CandidateEntity, Long>{
 
-//    public Specification<CandidateEntity> beforeAppliedDate(String path, String search) {
-//        return (root, cq, cb) -> {
-//            Path<?> pathToObject = this.resolvePathToObject(root, path);
-//            return this.getPredicateBeforeAppliedDate(cb, pathToObject, search);
-//        };
-//    }
-//    private Predicate getPredicateBeforeAppliedDate(CriteriaBuilder cb, Path<?> path, String search) {
-//            Expression<String> dateStringExpr = cb.function("TO_CHAR", String.class, path, cb.literal("'YYYY-MM-DD'"));
-//            return cb.like(cb.lower(dateStringExpr), this.getLowerCaseSearchTerm(search));
-//    }
-
-
     public Specification<CandidateEntity> beforeAppliedDate(String path, LocalDate search) {
         return (root, cq, cb) -> {
             Path<?> pathToDate = this.resolvePathToObject(root, path);
