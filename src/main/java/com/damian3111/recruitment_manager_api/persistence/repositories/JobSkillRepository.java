db5907a0d0fee625ef2058868da95a1e8cfd1e89
damian3111 <damiankwasny311@gmail.com>
2025-05-22 13:18:17 +0200

    Complete Logic JobSkillRepository

---
+++ src/main/java/com/damian3111/recruitment_manager_api/persistence/repositories/JobSkillRepository.java
@@ -0,0 +1,13 @@
+package com.damian3111.recruitment_manager_api.persistence.repositories;
+
+
+import com.damian3111.recruitment_manager_api.persistence.entities.CandidateSkill;
+import com.damian3111.recruitment_manager_api.persistence.entities.CandidateSkillId;
+import com.damian3111.recruitment_manager_api.persistence.entities.JobSkill;
+import com.damian3111.recruitment_manager_api.persistence.entities.JobSkillId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobSkillRepository extends JpaRepository<JobSkill, JobSkillId> {
+}
