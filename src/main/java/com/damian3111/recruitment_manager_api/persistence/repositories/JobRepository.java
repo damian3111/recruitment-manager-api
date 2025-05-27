d49c8eb243b640d40e2577ceca8f996dd027af0e
damian3111 <damiankwasny311@gmail.com>
2025-05-27 13:21:37 +0200

    JobRepository findByUserId

--- src/main/java/com/damian3111/recruitment_manager_api/persistence/repositories/JobRepository.java
+++ src/main/java/com/damian3111/recruitment_manager_api/persistence/repositories/JobRepository.java
@@ -1,14 +1,16 @@
 package com.damian3111.recruitment_manager_api.persistence.repositories;
 
+import com.damian3111.recruitment_manager_api.persistence.entities.CandidateEntity;
 import com.damian3111.recruitment_manager_api.persistence.entities.JobEntity;
 import com.damian3111.recruitment_manager_api.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface JobRepository extends JpaRepository<JobEntity, Long> {
+public interface JobRepository extends JpaRepository<JobEntity, Long>, JpaSpecificationExecutor<JobEntity> {
     Optional<List<JobEntity>> findByUserId(Long id);
 }
