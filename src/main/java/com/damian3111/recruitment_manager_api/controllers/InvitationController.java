bfe4bc24fc7617a71e233134db0e678fdf17d61c
damian3111 <damiankwasny311@gmail.com>
2025-06-05 20:53:28 +0200

    InvitationController logic exported to service layer

--- src/main/java/com/damian3111/recruitment_manager_api/controllers/InvitationController.java
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/InvitationController.java
@@ -1,23 +1,16 @@
 package com.damian3111.recruitment_manager_api.controllers;
 
 import com.damian3111.recruitment_manager_api.persistence.entities.InvitationEntity;
-import com.damian3111.recruitment_manager_api.persistence.repositories.InvitationRepository;
 import com.damian3111.recruitment_manager_api.services.InvitationService;
-import com.fasterxml.jackson.core.JsonParser;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.openapitools.api.InvitationsApi;
-import org.openapitools.api.JobsApi;
 import org.openapitools.model.DeleteInvitation200Response;
 import org.openapitools.model.InvitationDto;
-import org.openapitools.model.JobDto;
 import org.openapitools.model.UpdateInvitationStatusRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -45,23 +38,20 @@ public class InvitationController implements InvitationsApi {
 
     @Override
     public ResponseEntity<List<InvitationDto>> getInvitationsByRecruiterId(Long recruiterId) {
-        ResponseEntity<List<InvitationDto>> ok = ResponseEntity.ok(invitationService.getInvitationsByRecruiter(recruiterId).orElseThrow()
+        return ResponseEntity.ok(invitationService.getInvitationsByRecruiter(recruiterId).orElseThrow()
                 .stream().map(i -> modelMapper.map(i, InvitationDto.class)).collect(Collectors.toList()));
-        return ok;
     }
 
     @Override
     public ResponseEntity<InvitationDto> sendInvitation(InvitationDto invitationDto) {
         InvitationEntity invitation = invitationService.createInvitation(invitationDto);
-        InvitationDto map = modelMapper.map(invitation, InvitationDto.class);
-        return ResponseEntity.ok(map);
+        return ResponseEntity.ok(modelMapper.map(invitation, InvitationDto.class));
     }
 
     @Override
     public ResponseEntity<InvitationDto> updateInvitationStatus(Long id, UpdateInvitationStatusRequest updateInvitationStatusRequest) {
         InvitationEntity invitationEntity = invitationService.updateInvitationStatusById(id, updateInvitationStatusRequest);
-        InvitationDto invitationDto = modelMapper.map(invitationEntity, InvitationDto.class);
-        return ResponseEntity.ok(invitationDto);
+        return ResponseEntity.ok(modelMapper.map(invitationEntity, InvitationDto.class));
     }
 
     @Override
@@ -73,14 +63,12 @@ public class InvitationController implements InvitationsApi {
     @Override
     public ResponseEntity<List<InvitationDto>> getInvitationsByJobUserId(Long userId, String email) {
         List<InvitationEntity> invitationEntities = invitationService.getAcceptedInvitations(userId, email).orElseThrow();
-
         return ResponseEntity.ok(invitationEntities.stream().map(i -> modelMapper.map(i, InvitationDto.class)).collect(Collectors.toList()));
     }
 
     @Override
     public ResponseEntity<List<InvitationDto>> getInvitationsByEmail(Long userId, String email) {
         List<InvitationEntity> invitationEntities = invitationService.getInvitationsReceivedByRecruited2(userId, email).orElseThrow();
-
         return ResponseEntity.ok(invitationEntities.stream().map(i -> modelMapper.map(i, InvitationDto.class)).collect(Collectors.toList()));
     }
 
