dbaac86bc939bbc3cc32830af196872bc5ac03ed
damian3111 <damiankwasny311@gmail.com>
2025-06-05 20:52:22 +0200

    login/logout logic exported to AuthController

--- src/main/java/com/damian3111/recruitment_manager_api/controllers/AuthController.java
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/AuthController.java
@@ -3,14 +3,12 @@ package com.damian3111.recruitment_manager_api.controllers;
 import com.damian3111.recruitment_manager_api.persistence.entities.UserEntity;
 import com.damian3111.recruitment_manager_api.services.JWTService;
 import com.damian3111.recruitment_manager_api.services.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.openapitools.model.LoginUserDto;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -19,28 +17,27 @@ import java.util.HashMap;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-
     private final JWTService jwtService;
     private final UserService userService;
-    @Value("${security.jwt.secret-key}")
-    private String secretKey;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginUserDto loginUserDto, HttpServletResponse response) {
-        if (secretKey == null || secretKey.isEmpty()) {
-            System.err.println("JWT_SECRET is not set");
-            return ResponseEntity.status(500).body("Server configuration error");
-        }
-
+    public ResponseEntity<String> login(@RequestBody LoginUserDto loginUserDto, HttpServletResponse response){
         UserEntity userEntity = userService.authenticate(loginUserDto);
-        if (userEntity == null) {
-            return ResponseEntity.status(401).body("Invalid credentials");
-        }
-
         String jwtToken = jwtService.handleLogin(userEntity, response);
-        return ResponseEntity.ok()
-                .header("Set-Cookie", "authToken=" + jwtToken + "; HttpOnly; Secure; Path=/; Max-Age=86400; SameSite=None")
-                .body(jwtToken);
+        response.setHeader("Set-Cookie", "authToken=" + jwtToken + "; Path=/; Max-Age=3600; HttpOnly; Secure; SameSite=None");
+
+        return ResponseEntity.ok(jwtToken);
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie jwtCookie = new Cookie("authToken", null);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setSecure(true);
+        jwtCookie.setPath("/");
+        jwtCookie.setMaxAge(0);
+        response.addCookie(jwtCookie);
+
+        return ResponseEntity.ok("Logged out successfully");
+    }
 }
