366539685e140bdea21bdc345ada0aa810447d2c
damian3111 <damiankwasny311@gmail.com>
2025-06-05 20:53:04 +0200

    CandidateController logic exported to service layer

--- src/main/java/com/damian3111/recruitment_manager_api/controllers/CandidateController.java
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/CandidateController.java
@@ -1,32 +1,19 @@
 package com.damian3111.recruitment_manager_api.controllers;
 
 import com.damian3111.recruitment_manager_api.persistence.entities.CandidateEntity;
-import com.damian3111.recruitment_manager_api.persistence.entities.CandidateSkill;
-import com.damian3111.recruitment_manager_api.persistence.entities.SkillEntity;
-import com.damian3111.recruitment_manager_api.persistence.repositories.CandidateRepository;
-import com.damian3111.recruitment_manager_api.persistence.repositories.CandidateSkillRepository;
 import com.damian3111.recruitment_manager_api.persistence.specification.CandidatesSpecification;
 import com.damian3111.recruitment_manager_api.services.CandidateService;
-import com.damian3111.recruitment_manager_api.services.SkillService;
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.util.Strings;
 import org.modelmapper.ModelMapper;
 import org.openapitools.api.CandidatesApi;
-import org.openapitools.model.CandidateDto;
-import org.openapitools.model.CandidateFilter;
-import org.openapitools.model.CandidatesPage;
-import org.openapitools.model.JobDtoSkillsInner;
-import org.springframework.data.domain.Page;
+import org.openapitools.model.*;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -37,49 +24,47 @@ public class CandidateController implements CandidatesApi {
 
     private final CandidateService candidateService;
     private final ModelMapper modelMapper;
-    private final CandidatesSpecification candidatesSpecification;
-    private final SkillService skillService;
-    private final CandidateSkillRepository candidateSkillRepository;
+
     @Override
     public ResponseEntity<CandidateDto> addCandidate(CandidateDto candidateDto) {
-        CandidateEntity entity = modelMapper.map(candidateDto, CandidateEntity.class);
-        CandidateEntity savedEntity = candidateService.createCandidate(entity);
-        CandidateDto savedDto = modelMapper.map(savedEntity, CandidateDto.class);
-        return ResponseEntity.status(201).body(savedDto);
+        return ResponseEntity.status(201).body(modelMapper.map(candidateService.addCandidate(candidateDto), CandidateDto.class));
     }
 
     @Override
     public ResponseEntity<List<CandidateDto>> getAllCandidates() {
-        List<CandidateEntity> entities = candidateService.getAllCandidates();
-        List<CandidateDto> dtos = entities.stream()
+        return ResponseEntity.ok(candidateService.getAllCandidates().stream()
                 .map(entity -> modelMapper.map(entity, CandidateDto.class))
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(dtos);
+                .collect(Collectors.toList()));
     }
 
     @Override
     public ResponseEntity<CandidatesPage> filterCandidates(CandidateFilter filter, Pageable pageable) {
-        Page<CandidateEntity> entityPage = candidateService.getCandidatesFiltered(getSpecification(filter), pageable);
-        return ResponseEntity.ok(modelMapper.map(entityPage, CandidatesPage.class));
+        return ResponseEntity.ok(modelMapper.map(
+                candidateService.getCandidatesFiltered(filter, pageable), CandidatesPage.class
+        ));
     }
 
     @Override
     public ResponseEntity<CandidateDto> getCandidateById(Long id) {
-        log.info("Fetching candidate with ID: {}", id);
-        CandidateEntity candidateEntity = candidateService.getCandidateById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Candidate not found with ID: " + id));
-        return ResponseEntity.ok(mapToCandidateDto(candidateEntity));
+        try {
+            CandidateEntity candidate = candidateService.getCandidateById(id);
+            return ResponseEntity.ok(mapToCandidateDto(candidateService.getCandidateById(id)));
+        } catch (EntityNotFoundException e) {
+//            Map<String, String> errorResponse = new HashMap<>();
+//            errorResponse.put("message", null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @Override
     public ResponseEntity<CandidateDto> getCandidateByEmail(String email) {
-        CandidateEntity candidateEntity = candidateService.getCandidateByEmail(email).orElseThrow();
+        CandidateEntity candidateEntity = candidateService.getCandidateByEmail(email);
         CandidateDto candidateDto = modelMapper.map(candidateEntity, CandidateDto.class);
         candidateDto.setSkills(candidateEntity.getSkills().stream().map(s -> {
-            JobDtoSkillsInner jobDtoSkillsInner = new JobDtoSkillsInner();
-            jobDtoSkillsInner.setName(s.getSkill().getName());
-            jobDtoSkillsInner.setProficiencyLevel(JobDtoSkillsInner.ProficiencyLevelEnum.fromValue(s.getProficiencyLevel()));
-            return jobDtoSkillsInner;
+            CandidateDtoSkillsInner candidateDtoSkillsInner = new CandidateDtoSkillsInner();
+            candidateDtoSkillsInner.setName(s.getSkill().getName());
+            candidateDtoSkillsInner.setProficiencyLevel(CandidateDtoSkillsInner.ProficiencyLevelEnum.fromValue(s.getProficiencyLevel()));
+            return candidateDtoSkillsInner;
         }).collect(Collectors.toList()));
 
         return ResponseEntity.ok(candidateDto);
@@ -87,103 +72,15 @@ public class CandidateController implements CandidatesApi {
 
     @Override
     public ResponseEntity<CandidateDto> updateCandidate(Long id, CandidateDto candidateDto) {
-        CandidateEntity candidateEntity = candidateService.getCandidateByEmail(candidateDto.getEmail()).orElseThrow();
-        ArrayList<ArrayList> skillEntities = new ArrayList<>();
-
-        candidateDto.getSkills().forEach(s -> {
-            ArrayList<Object> objects = new ArrayList<>();
-            objects.add(skillService.findByName(s.getName()));
-            objects.add(s.getProficiencyLevel().getValue());
-            skillEntities.add(objects);
-        });
-
-        ArrayList<CandidateSkill> candidateSkills = new ArrayList<>();
-
-        skillEntities.forEach(s -> {
-            SkillEntity skill = (SkillEntity) s.get(0);
-            String proficiencyLevel = (String) s.get(1);
-            Optional<CandidateSkill> byCandidateIdAndSkillId = candidateSkillRepository.findByCandidateIdAndSkillId(candidateEntity.getId(), skill.getId());
-            if (byCandidateIdAndSkillId.isPresent()){
-                candidateSkillRepository.updateProficiencyLevel(candidateEntity.getId(), skill.getId(), proficiencyLevel);
-            }else {
-                CandidateSkill newSkill = CandidateSkill.builder()
-                        .skill(skill)
-                        .candidate(candidateEntity)
-                        .proficiencyLevel(proficiencyLevel)
-                        .build();
-                candidateSkillRepository.save(newSkill);
-            }
-            CandidateSkill candidateSkill = candidateSkillRepository.findByCandidateIdAndSkillId(candidateEntity.getId(), skill.getId()).orElseThrow();
-            candidateSkills.add(candidateSkill);
-        });
-
-        List<CandidateSkill> skills = candidateEntity.getSkills();
-        skills.clear();
-        skills.addAll(candidateSkills);
-
-        return ResponseEntity.ok(modelMapper.map(candidateService.updateCandidate(candidateEntity.getId(), candidateEntity, candidateDto), CandidateDto.class));
+        return ResponseEntity.ok(modelMapper.map(candidateService.updateCandidate(candidateDto), CandidateDto.class));
     }
 
-    private Specification<CandidateEntity> getSpecification(CandidateFilter filter) {
-        CandidatesSpecification factory = this.candidatesSpecification;
-        return Specification.where(Optional.ofNullable(filter.getFirstName())
-                        .map(v -> factory.propertyLikeIgnoreCase("firstName", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getLastName())
-                        .map(v -> factory.propertyLikeIgnoreCase("lastName", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getEmail())
-                        .map(v -> factory.propertyLikeIgnoreCase("email", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getPhone())
-                        .map(v -> factory.propertyLikeIgnoreCase("phone", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getHeadline())
-                        .map(v -> factory.propertyLikeIgnoreCase("headline", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getMinExperience())
-                        .map(v -> factory.propertyGreaterOrEqual("yearsOfExperience", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getMaxExperience())
-                        .map(v -> factory.propertyLessOrEqual("yearsOfExperience", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getEducation())
-                        .map(v -> factory.propertyLikeIgnoreCase("education", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getCertifications())
-                        .map(v -> factory.propertyLikeIgnoreCase("certifications", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getProjects())
-                        .map(v -> factory.propertyLikeIgnoreCase("projects", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getSalaryMin())
-                        .map(v -> factory.propertyGreaterOrEqual("salaryExpectation", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getSalaryMax())
-                        .map(v -> factory.propertyLessOrEqual("salaryExpectation", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getWorkStyle())
-                        .map(v -> factory.propertyLikeIgnoreCase("workStyle", v.getValue()))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getAppliedDateFrom())
-                        .map(v -> factory.afterAppliedDate("appliedDate", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getAppliedDateTo())
-                        .map(v -> factory.beforeAppliedDate("appliedDate", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getLocation())
-                        .map(v -> factory.propertyLikeIgnoreCase("location", v))
-                        .orElse(factory.empty()))
-                .and(Optional.ofNullable(filter.getSkills())
-                        .map(factory::propertyInSkills)
-                        .orElse(factory.empty()));
-    }
     private CandidateDto mapToCandidateDto(CandidateEntity candidateEntity) {
         CandidateDto candidateDto = modelMapper.map(candidateEntity, CandidateDto.class);
-        List<JobDtoSkillsInner> skills = candidateEntity.getSkills().stream()
-                .map(s -> new JobDtoSkillsInner()
+        List<CandidateDtoSkillsInner> skills = candidateEntity.getSkills().stream()
+                .map(s -> new CandidateDtoSkillsInner()
                         .name(s.getSkill().getName())
-                        .proficiencyLevel(JobDtoSkillsInner.ProficiencyLevelEnum.fromValue(s.getProficiencyLevel())))
+                        .proficiencyLevel(CandidateDtoSkillsInner.ProficiencyLevelEnum.fromValue(s.getProficiencyLevel())))
                 .collect(Collectors.toList());
         candidateDto.setSkills(skills);
         return candidateDto;
