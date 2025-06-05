a827053650cd69bd40068785bdbc40e6092947e3
damian3111 <damiankwasny311@gmail.com>
2025-06-05 20:54:34 +0200

    TestController - exported endpoints

--- src/main/java/com/damian3111/recruitment_manager_api/controllers/TestController.java
+++ src/main/java/com/damian3111/recruitment_manager_api/controllers/TestController.java
@@ -21,43 +21,8 @@ import java.util.HashMap;
 @RestController
 public class TestController {
 
-    private final JWTService jwtService;
-    private final UserService userService;
-
     @GetMapping("/test")
     public String helloWorld(){
         return "hello world!";
-    }
-
-    @PostMapping("/register")
-    public String helloWorld2(@RequestBody User user){
-        System.out.println(user);
-        System.out.println(user.getEmail());
-        System.out.println(user.getFirstName());
-        System.out.println(user.getUserRole());
-        System.out.println(user.getLastName());
-        return "hello world!";
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<String> helloWorld22(@RequestBody LoginUserDto loginUserDto, HttpServletResponse response){
-        UserEntity userEntity = userService.authenticate(loginUserDto);
-        String jwtToken = jwtService.handleLogin(userEntity, response);
-        response.setHeader("Set-Cookie", "authToken=" + jwtToken + "; Path=/; Max-Age=86400; HttpOnly; Secure; SameSite=None");
-        System.out.println("Set-Cookie header value: " + response.getHeader("Set-Cookie"));
-
-        return ResponseEntity.ok(jwtToken);
-    }
-
-    @PostMapping("/logout2")
-    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
-        Cookie jwtCookie = new Cookie("authToken", null);
-        jwtCookie.setHttpOnly(true);
-        jwtCookie.setSecure(true);
-        jwtCookie.setPath("/");
-        jwtCookie.setMaxAge(0);
-        response.addCookie(jwtCookie);
-
-        return ResponseEntity.ok("Logged out successfully");
     }
 }
