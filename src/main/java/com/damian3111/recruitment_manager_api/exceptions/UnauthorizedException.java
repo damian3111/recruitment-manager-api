2ebfe52ee4bc8f195fb7f99cf9fcb4460811d555
damian3111 <damiankwasny311@gmail.com>
2025-06-09 00:38:03 +0200

    add ForbiddenException, UnauthorizedException

---
+++ src/main/java/com/damian3111/recruitment_manager_api/exceptions/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package com.damian3111.recruitment_manager_api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
+
