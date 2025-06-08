9d5aa734c8184941c31735ede36f66d9a460926f
damian3111 <damiankwasny311@gmail.com>
2025-06-09 00:39:03 +0200

    refactot authentiate method

--- src/main/java/com/damian3111/recruitment_manager_api/services/UserService.java
+++ src/main/java/com/damian3111/recruitment_manager_api/services/UserService.java
@@ -1,5 +1,7 @@
 package com.damian3111.recruitment_manager_api.services;
 
+import com.damian3111.recruitment_manager_api.exceptions.ForbiddenException;
+import com.damian3111.recruitment_manager_api.exceptions.UnauthorizedException;
 import com.damian3111.recruitment_manager_api.persistence.entities.UserEntity;
 import com.damian3111.recruitment_manager_api.persistence.entities.UserRole;
 import com.damian3111.recruitment_manager_api.persistence.repositories.UserRepository;
@@ -10,6 +12,7 @@ import org.openapitools.model.User;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +30,8 @@ public class UserService {
         return userRepository.findUserEntityByEmail(email)
                 .orElseGet(() -> {
                     UserEntity user = new UserEntity();
+                    user.setFirstName("temporaryFirstName");
+                    user.setLastName("temporaryLastName");
                     user.setEmail(email);
                     user.setRole(UserRole.RECRUITER);
                     user.setPassword("testPassword");
@@ -51,22 +56,30 @@ public class UserService {
     }
 
     public UserEntity authenticate(LoginUserDto loginUserDto) {
-        try {
-            Authentication authentication = authenticationProvider.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginUserDto.getEmail(), loginUserDto.getPassword())
-            );
-        }catch (Exception e){
-                throw new RuntimeException("Wrong credentials");
-        }
         UserEntity userEntity = userRepository.findUserEntityByEmail(loginUserDto.getEmail())
-                .orElseThrow();
+                .orElseThrow(() -> new UnauthorizedException("Invalid email or password"));
+
+        if (!bCryptPasswordEncoder.matches(loginUserDto.getPassword(), userEntity.getPassword())) {
+            throw new UnauthorizedException("Invalid email or password");
+        }
 
         if (!userEntity.isEmailConfirmed()) {
-            throw new RuntimeException("Please confirm your email before logging in.");
+            throw new ForbiddenException("Please confirm your email before logging in.");
         }
+
+        try {
+            authenticationProvider.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginUserDto.getEmail(),
+                            loginUserDto.getPassword()
+                    )
+            );
+        } catch (AuthenticationException ex) {
+            throw new UnauthorizedException("Authentication failed");
+        }
+
         return userEntity;
     }
-
     public UserEntity getUserByEmail(String email) {
         return userRepository.findUserEntityByEmail(email).orElseThrow();
     }
